04b4d5cf61f90f605ffc69acf03b9f31f2acb528
nicholasb-17 <nicholas.bao@mail.mcgill.ca>
2026-07-24 12:14:52 -0400

    dataset update before feature implementation

--- dataset.py
+++ dataset.py
@@ -15,7 +15,7 @@ import torch
 
 def board_to_tensor(board: chess.Board) -> torch.Tensor:
     """
-    Converts a python-chess Board (object class built using bitboards) into an 21x8x8 numerical tensor (channel, height, width).
+    Converts a python-chess Board (object class built using bitboards) into an 19x8x8 numerical tensor (channel, height, width).
     Channels 0-5: White pieces (pawn, knight, bishop, rook, queen, king)
     Channels 6-11: Black pieces (same order)
     Channel 12: side to move (all 1s if White to move, all 0s if Black)
@@ -23,11 +23,10 @@ def board_to_tensor(board: chess.Board) -> torch.Tensor:
     Channels 15-16: Black kingside / queenside castling rights (all 1s or all 0s)
     Channel 17: en passant target square (1 at that square, 0 elsewhere)
     Channel 18: side to move is in check (all 1s or all 0s)
-    Channel 19: side to move is checkmated (all 1s or all 0s)
-    Channel 20: side to move is stalemated (all 1s or all 0s)
+    
     """
 #   Creating an empty tensor
-    tensor = np.zeros((21, 8, 8), dtype=np.float32)
+    tensor = np.zeros((19, 8, 8), dtype=np.float32)
 #   Mapping the board position to a dictionnary with keys from 0-63 (squares) and values as chess. Piece are objects with attributes .piece_type (from 1 to 6/ from pawn to king) and .color (True = White, False = Black)
     piece_map = board.piece_map()
 #   Filling the tensor with piece positions by iterating over the piece_map dictionary
@@ -54,16 +53,11 @@ def board_to_tensor(board: chess.Board) -> torch.Tensor:
         ep_rank, ep_file = divmod(board.ep_square, 8)
         tensor[17, ep_rank, ep_file] = 1.0
 
-#   Check / checkmate / stalemate: global facts about the side to move, so fill whole planes
+#   Check is a global facts about the side to move, so fill we can fill the whole channel with 1.0 if the side to move is in check, else 0.0
     tensor[18, :, :] = 1.0 if board.is_check() else 0.0
-    tensor[19, :, :] = 1.0 if board.is_checkmate() else 0.0
-    tensor[20, :, :] = 1.0 if board.is_stalemate() else 0.0
-
     return torch.from_numpy(tensor)
 
 #need to add:
 #board orientation
 #8 move history
-#is_game_over() (for checkmate, stalemate, insufficient material, 50-move rule, threefold repetition)
 #halfmove clock and fullmove number (for 50-move rule and game history) (normalized)
-#repitition count
